5c9e4b708e108a245975913ba685eb8bc9455736
Axolottl <sanilogia@gmail.com>
2021-07-13 00:11:03 +0200

    Leash Mechanic

---
+++ src/com/moderocky/misk/conditions/CondIsLeashed.java
@@ -0,0 +1,40 @@
+package me.sharpjaws.sharpsk.conditions;
+
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.Event;
+
+import javax.annotation.Nullable;
+
+public class CondIsLeashed extends Condition {
+    private Expression<Entity> en;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] expr, int matchedPattern, Kleenean arg2, SkriptParser.ParseResult arg3) {
+        en = (Expression<Entity>) expr[0];
+        return true;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "%entity% is leashed";
+    }
+
+    @Override
+    public boolean check(Event e) {
+
+        boolean check = false;
+        try {
+            LivingEntity en2 = (LivingEntity) en.getSingle(e);
+            check = en2.isLeashed();
+        } catch (NullPointerException ignored) {
+        }
+        return check;
+    }
+
+}
